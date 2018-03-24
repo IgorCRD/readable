cc577344f7b453d99461dff02479cdc6a15d019c
Igor Crispim Diniz <igor.crispimdiniz@gmail.com>
2018-03-24 13:09:24 -0300

    eslint config

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const path = require("path");
-const Express = require("express");
-const cors = require("cors");
-const servingConfig = require("./serving-config");
+const path = require('path');
+const Express = require('express');
+const cors = require('cors');
+const servingConfig = require('./serving-config');
 
 const app = new Express();
 const port = process.env.PORT || 8080;
 
-const distDir = path.join(__dirname, "dist");
+const distDir = path.join(__dirname, 'dist');
 
 app.use(cors(servingConfig.corsOptions));
 
 app.use(Express.static(distDir));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(distDir, "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distDir, 'index.html'));
 });
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log("Server listening on port %s, Ctrl+C to stop", port);
+  console.log('Server listening on port %s, Ctrl+C to stop', port);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,75 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.[hash].js",
-    publicPath: "/"
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.[hash].js',
+    publicPath: '/',
   },
   resolve: {
     alias: {
-      root: path.resolve(__dirname, "./"),
-      src: path.resolve(__dirname, "src/")
+      root: path.resolve(__dirname, './'),
+      src: path.resolve(__dirname, 'src/'),
     },
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx'],
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   module: {
     rules: [
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "eslint-loader",
+        loader: 'eslint-loader',
         options: {
           emitError: true,
           emitWarning: true,
           failOnWarning: true,
-          failOnError: true
-        }
+          failOnError: true,
+        },
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
       },
       {
         test: /\.(jpg|png|gif|svg|pdf|ico)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[path][name]-[hash:8].[ext]"
-            }
-          }
-        ]
-      }
-    ]
+              name: '[path][name]-[hash:8].[ext]',
+            },
+          },
+        ],
+      },
+    ],
   },
   devServer: {
     historyApiFallback: {
-      rewrites: [{ from: ".", to: "./dist/index.html" }]
+      rewrites: [{ from: '.', to: './dist/index.html' }],
     },
     compress: true,
-    disableHostCheck: true
+    disableHostCheck: true,
   },
   plugins: [
     new HtmlWebPackPlugin({
-      favicon: "./assets/images/favicon.ico",
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
+      favicon: './assets/images/favicon.ico',
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
